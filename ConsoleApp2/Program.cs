20ff8341a09ac73e4cdb7c4c8e1372fc6f3962d2
Венера Салихова <venya2207@mail.ru>
2018-12-27 01:20:02 +0400

    Добавьте файлы проекта.

---
+++ ConsoleApp2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var val = int.Parse(Console.ReadLine());
+            if (val < 1) return;
+            string rub = val.ToString(), rubleCase = "рублей";
+            char lastChar = ruble[ruble.Length - 1];
+            if (ruble.Length >= 2 && ruble[ruble.Length - 2] == '1')
+            {
+                rubleCase = "рублей";
+            }
+            else if (lastChar == '1')
+            {
+                rubleCase = "рубль";
+            }
+            else if (lastChar == '2' || lastChar == '3' || lastChar == '4')
+            {
+                rubleCase = "рубля";
+            }
+            Console.WriteLine("{0} {1}", val, rubleCase);
+            Console.ReadKey();
+        }
+    }
+}
